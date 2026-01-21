42737ef219012b20320b6de524a8b65c0e553592
ragan <ragan@EERIEBOOK>
2026-01-21 20:43:18 +0100

    Update.

--- Assets/Resources/Source/Serialization.cs
+++ Assets/Resources/Source/Serialization.cs
@@ -83,13 +83,13 @@ class Serialization
             sw.WriteLine("-");
             sw.WriteLine("-");
             sw.WriteLine("Tracklist:");
-            sw.WriteLine("");
+            sw.WriteLine("-");
             sw.WriteLine("here1");
             sw.WriteLine("3:23");
             sw.WriteLine("here2");
             sw.WriteLine("6:51");
             sw.WriteLine("here3");
-            sw.WriteLine("12:25");
+            sw.Write("12:25");
         }
         if (useUnityData)
             Process.Start(prefix + "MooRT_Data_2/" + file + ".txt");
